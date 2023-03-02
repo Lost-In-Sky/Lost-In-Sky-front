3eac1b78d437c340b9473765bcea929b833d1bfa
VladMeliksetyan <vladimirmeliksetyan2@gmail.com>
2023-03-02 10:12:37 +0400

    Create card component and remove unneccessary parts form code

---
+++ src/Components/HotelCard/HotelCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import { CardContainr } from './HotelCard.style';
+
+const HotelCard = () => {
+    return (
+        <CardContainr>
+            <Button variant='contained'>
+                Hello
+            </Button>
+        </CardContainr>
+
+
+    )
+}
+
+export default HotelCard
---
+++ src/Components/HotelCard/HotelCard.style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const CardContainr = styled.div`
+display:flex;
+flex-direction:column;
+width:5rem;
+height:7rem; 
+justify:content:center;
+border:3px black;
+`
+
+export { CardContainr }
