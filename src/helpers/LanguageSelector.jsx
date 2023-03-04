d3d1f275400252f6a12380ccdd5a723865b4aae6
Misak <misaksofoyan@gmail.com>
2023-03-04 16:54:36 +0400

    languageSelector add pointer to icons + remove unnecessary imports

--- src/helpers/LanguageSelector.jsx
+++ src/helpers/LanguageSelector.jsx
@@ -1,14 +1,12 @@
 import { LngCont } from "../components/Navbar/Navbar.styles";
-import { useTranslation } from "react-i18next";
 
 export const LanguageSelector = ({ onLanguageChange }) => {
-  const { t } = useTranslation();
 
   return (
     <LngCont>
-      <p onClick={() => onLanguageChange("am")}>🇦🇲</p>
-      <p onClick={() => onLanguageChange("en")}>🇺🇸</p>
-      <p onClick={() => onLanguageChange("ru")}>🇷🇺</p>
+      <p style={{ cursor: 'pointer'}} onClick={() => onLanguageChange("am")}>🇦🇲</p>
+      <p style={{ cursor: 'pointer'}} onClick={() => onLanguageChange("en")}>🇺🇸</p>
+      <p style={{ cursor: 'pointer'}} onClick={() => onLanguageChange("ru")}>🇷🇺</p>
     </LngCont>
   );
 };
