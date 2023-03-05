a10956a829a788dd8958a5ee37397d948149c08c
VladMeliksetyan <vladimirmeliksetyan2@gmail.com>
2023-03-05 22:26:00 +0400

    final style

--- src/components/Footer/Footer.style.js
+++ src/components/Footer/Footer.style.js
@@ -10,14 +10,14 @@ export const FooterContainer = styled.footer`
   flex-direction: column;
   text-align: center;
 
-  h3{
+  h3 {
     font-size: 2.1rem;
     font-weight: 500;
     text-transform: capitalize;
     line-height: 3rem;
   }
 
-  ul{
+  ul {
     list-style-type: none;
     padding: 0;
     display: flex;
@@ -34,9 +34,9 @@ export const FooterContainer = styled.footer`
   }
 
   .instagram-icon {
-    a{
-      color: white
-    } 
+    a {
+      color: white;
+    }
   }
 `;
 
@@ -49,28 +49,4 @@ export const Wrapper = styled.div`
       color: #ccc;
     }
   }
-=======
-  a{
-    text-decoration: none;
-    color: white;
-  }
-  width: 100%;
-
-  .mapouter {
-    background-color: #a9a9a9 ;
-    color: white;
-  }
-
-  .gmap_canvas {
-    gap: 1.5rem;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-  }
-  
-`;
-
-export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
 `;
