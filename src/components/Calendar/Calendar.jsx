9ece6de3df23ddc3df65ea0d599247a5c7b65e52
MisakSofoyan1 <99541163+MisakSofoyan1@users.noreply.github.com>
2023-03-07 13:02:10 +0400

    Update Calendar.jsx

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,38 +1,50 @@
 import Calendar from "react-calendar"
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useTransition } from "react"
 import 'react-calendar/dist/Calendar.css';
+import hy from "./hy";
 
 function CalendarComponent() {
+    // NEED to implement the language query to get the current language and change the calendar language
 
-  const [dateRange, setDateRange] = useState([]);
 
-  const disabledDates = [
-    new Date(2023, 2, 7),
-    new Date(2023, 2, 10),
-    new Date(2023, 2, 11),
-    new Date(2023, 2, 12)
-  ];
+    const [dateRange, setDateRange] = useState([]);
 
-  const handleRangeSelection = (dateRange) => {
-    setDateRange(dateRange);
-  }
+    const disabledDates = [
+        new Date(2023, 2, 7),
+        new Date(2023, 2, 10),
+        new Date(2023, 2, 11),
+        new Date(2023, 2, 12)
+    ];
 
-  const isDateDisabled = (date) => {
-    return disabledDates.some(disabledDate =>
-      disabledDate.getTime() === date.date.getTime()
-    );
-  }
+    const handleRangeSelection = (dateRange) => {
+        setDateRange(dateRange);
+    }
 
-  return (
-    <div>
-      <Calendar
-        selectRange={true}
-        onChange={handleRangeSelection}
-        tileDisabled={isDateDisabled}
-        locale="ru-RU"
-      />
-    </div>
-  )
+    const isDateDisabled = (date) => {
+        return disabledDates.some(disabledDate =>
+            disabledDate.getTime() === date.date.getTime()
+        );
+    }
+
+    return (
+        <div>
+            <Calendar
+                selectRange={true}
+                onChange={handleRangeSelection}
+                tileDisabled={isDateDisabled}
+                locale={hy.locale}
+                formatShortWeekday={(locale, value) =>
+                    hy.weekdaysShort[value.getDay()]
+                  }
+                formatMonthYear={(locale, value) =>
+                    `${hy.months[value.getMonth()]} ${value.getFullYear()}`
+                  }
+                formatWeekday={(locale, value) =>
+                    hy.weekdaysLong[value.getDay()]
+                  }
+            />
+        </div>
+    )
 }
 
 export default CalendarComponent
