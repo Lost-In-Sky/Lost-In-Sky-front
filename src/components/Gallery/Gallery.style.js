1c599f628140576741f3cd818301c412788a5e03
Misak <misaksofoyan@gmail.com>
2023-03-19 11:02:49 +0400

    galery

--- src/components/Gallery/Gallery.style.js
+++ src/components/Gallery/Gallery.style.js
@@ -7,10 +7,37 @@ export const ImagesWrapper = styled.div`
   gap: 1rem;
 
   img {
-    width: 100%;
-    max-width: 300px;
-    height: auto;
+    height: 600px;
     object-fit: cover;
     border-radius: 8px;
+    cursor: pointer;
+
+    @media (max-width: 1320px) {
+      max-width: 95%;
+      margin-left: 2.5%;
+    }
+  }
+`;
+
+export const SelectedImageWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+
+
+
+  .selected {
+    max-height: 90vh;
+    max-width: 90vw;
+    object-fit: contain;
+    border-radius: 8px;
+    cursor: pointer;
   }
 `;
